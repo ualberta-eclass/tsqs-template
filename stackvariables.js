a27a099efbdd78b5fa18b00b24aaa11d9abc3eef
aazizua <30669231+aazizua@users.noreply.github.com>
2019-07-10 12:23:55 -0600

    Initial Commit

---
+++ stackvariables.js
@@ -0,0 +1,11 @@
+module.exports.variables = () => {
+    let config = {
+        service: "tsqs-template",
+        region: 'us-west-2',
+        mysqluser: 'master',
+        mysqlpassword: '',
+        mysqldb: 'testdb',
+        mysqlhost: ''
+    }
+    return config
+};
